59215a5889fc6e81293d32e43d7852c89b461fa7
Vinicius Monteiro <vinicius.hm@globo.com>
2020-10-17 17:24:17 -0300

    Criação do projeto de teste

---
+++ CP.APi.Usuario.TesteUnitario/Controller/UsuarioControllerTestes.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace CP.APi.Usuario.TesteUnitario
+{
+    public class UsuarioControllerTestes
+    {
+        [Test]
+        public void DeveObterTodosUsuarios()
+        {
+            var controller  = new UsuarioController();
+        }
+    }
+}
